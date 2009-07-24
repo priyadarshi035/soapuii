bef91b57065f467990550e565efcfc898c4cb21e
Andrzej Zaleski <azl@touk.pl>
2009-07-24 12:01:27 +0000

    adding deploy-parser
    git-svn-id: file:///home/users/z/soapuii/svn-backup/soapuii/trunk@12866 a7b80be8-48eb-4681-8a4f-b7484ab9f0e7

---
+++ deploy-parser-core/src/main/java/pl/touk/deployparser/App.java
@@ -0,0 +1,13 @@
+package pl.touk.deployparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
