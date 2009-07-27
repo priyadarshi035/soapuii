2b0331871a865f65360bd08a685b37522b65accd
Piotr Nowojski <pnw@touk.pl>
2009-07-27 13:30:16 +0000

    git-svn-id: file:///home/users/z/soapuii/svn-backup/soapuii/trunk@12885 a7b80be8-48eb-4681-8a4f-b7484ab9f0e7

--- soapuii/src/java/com/eviware/soapui/impl/wsdl/actions/iface/RenameInterfaceAction.java
+++ soapuii/src/java/com/eviware/soapui/impl/wsdl/actions/iface/RenameInterfaceAction.java
@@ -1,16 +1,4 @@
 /*
- * soapUI, copyright (C) 2004-2009 eviware.com
- *
- * soapUI is free software; you can redistribute it and/or modify it under the
- * terms of version 2.1 of the GNU Lesser General Public License as published by
- * the Free Software Foundation.
- *
- * soapUI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details at gnu.org.
- */
-
-/*
  *  soapUI, copyright (C) 2004-2009 eviware.com 
  *
  *  soapUI is free software; you can redistribute it and/or modify it under the 
@@ -26,15 +14,14 @@ package com.eviware.soapui.impl.wsdl.actions.iface;
 
 
 import com.eviware.soapui.impl.wsdl.WsdlInterface;
-import com.eviware.soapui.impl.wsdl.WsdlProject;
 import com.eviware.soapui.support.MessageSupport;
 import com.eviware.soapui.support.UISupport;
 import com.eviware.soapui.support.action.support.AbstractSoapUIAction;
 
 /**
- * Action for creating a new WSDL project
+ * Action for renaming an existing WSDL project
  * 
- * @author Ole.Matzura
+ * @author Piotr Nowojski
  */
 
 public class RenameInterfaceAction extends AbstractSoapUIAction<WsdlInterface>
