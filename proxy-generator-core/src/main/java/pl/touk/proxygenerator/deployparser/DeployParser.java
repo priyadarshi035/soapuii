6e1c951cd9515948239a6e594fc11b49f0086fa8
Andrzej Zaleski <azl@touk.pl>
2009-07-27 08:42:30 +0000

    git-svn-id: file:///home/users/z/soapuii/svn-backup/soapuii/trunk@12873 a7b80be8-48eb-4681-8a4f-b7484ab9f0e7

--- proxy-generator-core/src/main/java/pl/touk/proxygenerator/deployparser/DeployParser.java
+++ proxy-generator-core/src/main/java/pl/touk/proxygenerator/deployparser/DeployParser.java
@@ -4,10 +4,15 @@
  */
 
 package pl.touk.proxygenerator.deployparser;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import javax.xml.parsers.*;
 import javax.xml.xpath.*;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -18,17 +23,29 @@ public class DeployParser implements DeployParserInterface
 	private final static Pattern namespacePrefixPattern = Pattern.compile("(\\w+):(\\w+)");
 	private final static String deployFileName = "deploy.xml";
 	private final static Logger log = Logger.getLogger(DeployParser.class.getName());
-	private DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-	private DocumentBuilder builder;
+	private DocumentBuilderFactory domBuilderFactory = DocumentBuilderFactory.newInstance();
+	private DocumentBuilder domBuilder;
+	private Document dom;
+
 	private XPathFactory factory = XPathFactory.newInstance();
 
 	public DeployParser() throws ParserConfigurationException
 	{
-		domFactory.setNamespaceAware(true);
-		domFactory.setIgnoringComments(true);
-		builder = domFactory.newDocumentBuilder();
+		domBuilderFactory.setNamespaceAware(true);
+		domBuilderFactory.setIgnoringComments(true);
+		domBuilder = domBuilderFactory.newDocumentBuilder();
 	};
 
+	public void ParseXML(File newFile) throws SAXException
+	{
+		try {
+			dom = domBuilder.parse(newFile);
+//		Document doc = docBuilder.parse (new File("test.xml"));
+		} catch (IOException ex) {
+			Logger.getLogger(DeployParser.class.getName()).log(Level.SEVERE, null, ex);
+		}
+//		Document doc = docBuilder.parse (new File("test.xml"));
+	}
 
 
 }
